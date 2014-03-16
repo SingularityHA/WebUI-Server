a5ab80db9d81518fa2dbb145b78512bfb892826a
Dan Faulknor <daniel@zzrv.co>
2014-03-16 15:46:04 +1300

    Updated to send state changes into MQTT actuators

--- pusher.js
+++ pusher.js
@@ -4,19 +4,30 @@ var mqtt = require('mqtt');
  
 var io  = require('socket.io').listen(5000);
 client = mqtt.createClient(1883, '172.25.119.224');
+
  
 io.sockets.on('connection', function (socket) {
-  socket.on('subscribe', function (data) {
+socket.on('subscribe', function (data) {
     console.log('Subscribing to '+data.topic);
     client.subscribe(data.topic);
   });
+
   socket.on('command', function(data) {
-	console.log(data);
+	console.log(data)
+	if ("state" in data) {
+		client.publish('actuators', '[ "' + data['module'] + '","' + data['device'] + '","' + data['state'] + '"]') 
+	}
+	else {
+		client.publish('actuators', '[ "' + data['module'] + '","' + data['device'] + '","' + data['attribute'] + '","' + data['value'] + '"]')
+	}
   });
+
+}); 
+
+client.on('message', function(topic, message) {
+	console.log(message)
+	io.sockets.emit(topic, message);
 
 });
 
 
-client.on('message', function(topic, message) {
-	io.sockets.emit(topic, message);
-}); 
